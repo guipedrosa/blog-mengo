bd013358c480674034b738ac13143205313e52f5
Guilherme Lima <gui.pedrosa@gmail.com>
2023-07-22 22:24:45 -0300

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+use Illuminate\Support\Str;
+
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'slug', 'thumbnail', 'body', 'active', 'published_at', 'user_id'];
+
+    protected $casts = [
+        'published_at' => 'datetime'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function getPublishedDate()
+    {
+        return $this->published_at->format('d/m/y H:i:s');
+    }
+
+    public function shortBody()
+    {
+        return Str::words(strip_tags($this->body), 30);
+    }
+
+}
