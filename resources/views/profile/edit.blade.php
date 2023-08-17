528379efdd23ea2b59cc047f8eff4ac55ce32b71
Guilherme Lima <gui.pedrosa@gmail.com>
2023-08-17 15:30:45 -0300

    change user attributes, change profile user changes flash message

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,12 +1,16 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+        
+    <div class="py-12">        
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            
+            <div class="text-xl px-8">{{ __('Edição de Perfil') }}</div>
+
+            @if (session('status'))
+                <div class="text-xl ml-8 p-2 text-white bg-green-600 rounded-lg" x-data="{show: true}" x-show="show" x-init="setTimeout(() => show = false, 5000)">
+                    {{ __('Perfil salvo com sucesso!') }}
+                </div>
+            @endif
+
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
@@ -26,4 +30,5 @@
             </div>
         </div>
     </div>
+
 </x-app-layout>
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,11 +1,11 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Profile Information') }}
+            {{ __('Meus dados') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __("Update your account's profile information and email address.") }}
+            {{ __("Atualize seu nome e/ou email.") }}
         </p>
     </header>
 
