2c8836bb4dfb7a328a3599686e3f349c914458ca
Guilherme Lima <gui.pedrosa@gmail.com>
2023-08-16 21:22:02 -0300

    improve product view

--- resources/views/livewire/product-price.blade.php
+++ resources/views/livewire/product-price.blade.php
@@ -1,8 +1,8 @@
 <div wire:poll>
     {{-- Do your work, then step back. --}}
-    <p class="font-bold px-4 py-3 text-xl">Preço Inicial: <strike>R$1000,00</strike></p>
-    Preço:<span class="font-bold px-4 py-3 text-xl"> R${{ $product->getInitialPrice() }} </span>    
     <p>
-        Current time: {{ now() }}
-    </p>   
+        {{ __('Por') }}: <span class="font-bold py-3 text-xl"> R${{ $product->getInitialPrice() }}</span>    
+        <br />
+        <div class="mt-3">{{ date('d/m/Y H:i:s') }}</div>
+    </p>
 </div>
